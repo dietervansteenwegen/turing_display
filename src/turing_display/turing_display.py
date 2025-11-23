a9bf8003d0f767bb68a8fd2fad070dab283f306a
Dieter Vansteenwegen <github@vansteenwegen.org>
2025-11-23 23:03:05 +0100

    feat: Add color bar testing pattern

--- src/turing_display/turing_display.py
+++ src/turing_display/turing_display.py
@@ -44,12 +44,8 @@ class TuringDisplay:
     def initialize_display(self, reset: bool = True, brightness: int = 127):
         if reset:
             self.lcd.reset()
-        # Send initialization commands
         self.lcd.initialize_comm()
-        # Turn on display, set brightness and LEDs for supported HW
         self.turn_on(brightness=brightness)
-
-        # Set orientation
         self.lcd.set_orientation(self._orientation)
 
     def turn_on(
@@ -57,25 +53,18 @@ class TuringDisplay:
         brightness: Optional[int] = None,
         display_test_pattern: Optional[bool] = False,
     ):
-        # Turn screen on in case it was turned off previously
         self.lcd.screen_on()
 
         if brightness:
-            # Set brightness
             self.lcd.set_brightness(brightness)
         if display_test_pattern:
-            # Display test pattern
             self.display_test_pattern()
-        # Set backplate RGB LED color (for supported HW only)
         # self.lcd.set_backplate_led_color(
         # config.THEME_DATA['display'].get('DISPLAY_RGB_LED', (255, 255, 255))
         # )
 
     def turn_off(self):
-        # Turn screen off
         self.lcd.screen_off()
-
-        # Turn off backplate RGB LED
         # self.lcd.set_backplate_led_color(led_color=(0, 0, 0))  # TODO, check supported for all
 
     def display_test_pattern(self):
@@ -84,18 +73,19 @@ class TuringDisplay:
             bitmap_path=str(fn), x=0, y=0, width=self._width, height=self._height
         )
 
-    # def display_static_image(self):
-    #     if config.THEME_DATA.get('static_images', False):
-    #         for image in config.THEME_DATA['static_images']:
-    #             log.debug(f'Drawing Image: {image}')
-    #             self.lcd.display_bitmap(
-    #                 bitmap_path=config.THEME_DATA['PATH']
-    #                 + config.THEME_DATA['static_images'][image].get('PATH'),
-    #                 x=config.THEME_DATA['static_images'][image].get('X', 0),
-    #                 y=config.THEME_DATA['static_images'][image].get('Y', 0),
-    #                 width=config.THEME_DATA['static_images'][image].get('WIDTH', 0),
-    #                 height=config.THEME_DATA['static_images'][image].get('HEIGHT', 0),
-    #             )
+    def display_test_text(self):
+        self.lcd.display_text(
+            text='Turing Display\nTest Pattern',
+            x=0,
+            y=0,
+            width=self._width,
+            height=self._height,
+            font_size=24,
+            font_color=(255, 0, 0),
+            background_color=(0, 0, 0),
+            align='center',
+            anchor='mm',
+        )
 
     # def display_static_text(self):
     #     if config.THEME_DATA.get('static_text', False):
@@ -112,7 +102,8 @@ class TuringDisplay:
     #                     'FONT', 'roboto-mono/RobotoMono-Regular.ttf'
     #                 ),
     #                 font_size=config.THEME_DATA['static_text'][text].get('FONT_SIZE', 10),
-    #                 font_color=config.THEME_DATA['static_text'][text].get('FONT_COLOR', (0, 0, 0)),
+    #                 font_color=config.THEME_DATA['static_text'][text].get('FONT_COLOR',
+    # (0, 0, 0)),
     #                 background_color=config.THEME_DATA['static_text'][text].get(
     #                     'BACKGROUND_COLOR', (255, 255, 255)
     #                 ),
