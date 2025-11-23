d156d033c481994265dd7646a6180f8f0b059c9e
Dieter Vansteenwegen <github@vansteenwegen.org>
2025-11-23 23:01:56 +0100

    feat: Add basic logging during debug

--- src/turing_display/__main__.py
+++ src/turing_display/__main__.py
@@ -12,10 +12,23 @@ from . import TuringDisplay  # noqa: F401
 
 def test_display():
     """Test function to initialize and test the Turing display."""
-    display = TuringDisplay(port='/tty/ttyACM0', width=480, height=320)
+    display = TuringDisplay(port='AUTO', width=480, height=320)
     display.initialize_display(reset=True, brightness=100)
     display.turn_on(display_test_pattern=True)
+    display.display_test_text()
+
+
+def check_logging_requirements():
+    """Check if logging requirements are met."""
+    import logging
+    import os
+
+    log_level = os.getenv('ENABLE_TURING_DISPLAY_DEBUG', 'FALSE').upper()
+    if log_level == 'TRUE':
+        print('Enabling debug logging for turing_display')
+        logging.basicConfig(level=logging.DEBUG)
 
 
 if __name__ == '__main__':
+    check_logging_requirements()
     test_display()
